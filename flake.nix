6229f07316f9e7f0b1109ff6a978880d503bf878
sercus <sercus@users.noreply.github.com>
2025-06-15 01:24:31 +0000

    chore(deps): update flake.lock

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Hydenix's nixpkgs
-    hydenix-nixpkgs.url = "github:nixos/nixpkgs/ecd26a469ac56357fd333946a99086e992452b6a";
+    hydenix-nixpkgs.url = "github:nixos/nixpkgs/6afe187897bef7933475e6af374c893f4c84a293";
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
